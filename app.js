542d42af35227297bb3dda5076587943af38accb
PhamHa103 <havoia1@gmail.com>
2022-07-12 00:21:22 +0700

    edit function

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-function validate(name, birthday, phone) {
+let editingRowId = "";
+function validate(name, birthday, phone, hometown) {
     const result = {
         name: true,
         birthday: true,
-        phone: true
+        phone: true,
+        hometown: true
     }
     if (name.length > 50 || name === "")
         result.name = false;
@@ -11,6 +13,8 @@ function validate(name, birthday, phone) {
     if (birthday === "" || now <= birthdayDate || now.getFullYear() - birthdayDate.getFullYear() > 120)
         result.birthday = false;
     if (phone.length != 10 || !/^[0-9]+$/.test(phone))
+        result.phone = false;
+    if (hometown === "")
         result.phone = false;
     return result;
 }
@@ -55,22 +59,39 @@ function addStudent(name, birthday, phone, hometown) {
     `
     $("#tableBody").append(studentRow);
     onChangeCheckbox(idOfRow);
+    alert("Add success!");
+}
+
+function editStudent(name, birthday, phone, hometown) {
+    const editRow = getEditRow();
+    const editCols = $(editRow).children();
+    editCols[1].innerText = name;
+    editCols[2].innerText = birthday;
+    editCols[3].innerText = phone;
+    editCols[4].innerText = hometown;
+    editingRowId = "";
+    alert("Edit success!");
 }
 $("#buttonSave").click(function () {
     const name = $("#name").val();
     const birthday = $("#birthday").val();
     const phone = $("#phone").val();
     const hometown = $("#hometown").val();
-    const validateResult = validate(name, birthday, phone);
+    const validateResult = validate(name, birthday, phone, hometown);
     if (Object.values(validateResult).every(
         function (value) { return value; }
     )) {
-        addStudent(name, birthday, phone, hometown);
-        $("#buttonReset").click();
         // add or edit
+        if (editingRowId) {
+            editStudent(name, birthday, phone, hometown);
+        } else {
+            addStudent(name, birthday, phone, hometown);
+        }
+        $("#buttonReset").click();
         $("#nameError").hide();
         $("#birthdayError").hide();
         $("#phoneError").hide();
+        $("#hometownError").hide();
     } else {
         if (!validateResult.name) {
             $("#nameError").show();
@@ -87,16 +108,44 @@ $("#buttonSave").click(function () {
         } else {
             $("#phoneError").hide();
         }
+        if (!validateResult.hometown) {
+            $("#hometownError").show();
+        } else {
+            $("#hometownError").hide();
+        }
     }
 })
 $("#buttonDelete").click(function () {
-    const checkedInputs = $("input:checked");
-    for (let index = 0; index < checkedInputs.length; index++) {
-        const element = checkedInputs[index];
-        const rowElement = $(element).parent().parent();
-        $(rowElement).remove();
+    let text = "Do you want to delete?";
+    if (confirm(text) == true) {
+        const checkedInputs = $("input:checked");
+        for (let index = 0; index < checkedInputs.length; index++) {
+            const element = checkedInputs[index];
+            const rowElement = $(element).parent().parent();
+            $(rowElement).remove();
+        }
+        alert("Delete success!");
     }
 })
 
-
+function getEditRow() {
+    const checkedInputs = $("input:checked");
+    for (let index = 0; index < checkedInputs.length; index++) {
+        const element = checkedInputs[index];
+        return $(element).parent().parent()[0];
+    }
+}
+$("#buttonEdit").click(function () {
+    if (countCheckedRow() >= 2) {
+        alert("You must select only one checkbox!");
+    } else {
+        const editRow = getEditRow();
+        editingRowId = editRow.id;
+        const editCols = $(editRow).children();
+        $("#name").val(editCols[1].innerText);
+        $("#birthday").val(editCols[2].innerText);
+        $("#phone").val(editCols[3].innerText);
+        $("#hometown").val(editCols[4].innerText);
+    }
+})
 
